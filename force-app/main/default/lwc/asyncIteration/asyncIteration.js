4cc9f1cfec3427ddc2791f82663f9c10caee9ef8
Alba Rivas <arivas@salesforce.com>
2021-02-22 09:20:33 +0100

    Remove intl examples Improve for await example

--- force-app/main/default/lwc/asyncIteration/asyncIteration.js
+++ force-app/main/default/lwc/asyncIteration/asyncIteration.js
@@ -2,20 +2,45 @@ import { LightningElement } from "lwc";
 
 export default class AsyncIteration extends LightningElement {
   async asyncIteration() {
-    const iterables = [
-      new Promise((resolve) => setTimeout(resolve("Astro"), 100)),
-      "Codey",
-      new Promise((resolve) => setTimeout(resolve("Appy"), 100))
-    ];
 
-    for await (const obj of iterables) {
-      console.log(obj);
+    function sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
     }
 
+    const iterables = [
+      sleep(1000).then(() => { console.log("Promise 1 resolved"); return "Astro";}),
+      sleep(3000).then(() => { console.log("Promise 2 resolved"); return "Codey";}),
+      sleep(2000).then(() => { console.log("Promise 3 resolved"); return "Appy";})
+    ];
+
+    const mascots = [];
+    const startTimestamp = Date.now();
+    for await (const obj of iterables) {
+      console.log(`${obj} arrived in ${Date.now() - startTimestamp} ms!`);
+      mascots.push(obj);
+    }
+
+    console.log(`These mascots went to the party:${mascots.join(',')}`);
+
     /* expected ouput
-        Astro
-        Codey
-        Appy
+        Promise 1 resolved
+        Astro arrived in 1000 ms!
+        Promise 3 resolved
+        Promise 2 resolved
+        Codey arrived in 3000 ms!
+        Appy arrived in 3000 ms!
+        These mascots went to the party:Astro,Codey,Appy
+    */
+
+    /*
+    const otherIterables = [
+      sleep(1000).then(() => { console.log("Other promise 1 resolved"); return "Cloudy";}),
+      sleep(3000).then(() => { console.log("Other promise 2 resolved"); return "Einstein";}),
+      sleep(2000).then(() => { console.log("Other promise 3 resolved"); return "Blaze";})
+      ]
+
+    const startTimestamp2 = Date.now();
+    Promise.all(otherIterables).then(results => {console.log(`${results} arrived in ${Date.now() - startTimestamp2} ms!`)});
     */
   }
 
--- force-app/main/default/lwc/promiseFinally/promiseFinally.js
+++ force-app/main/default/lwc/promiseFinally/promiseFinally.js
@@ -22,6 +22,24 @@ export default class PromiseFinally extends LightningElement {
   }
 
   get code() {
-    return this.promiseFinally.toString();
+    return `promiseFinally() {
+      Promise.resolve("Salesforce mascots are super fun!")
+        .then((value) => console.log(value))
+        .catch((error) => console.error(error))
+        .finally(() => console.log("Yes, they definitely are"));
+      /* expected output:
+          Salesforce mascots are super fun!
+          Yes, they definitely are
+      */
+  
+      Promise.reject("Learning new JavaScript features is hard")
+        .then((value) => console.log(value))
+        .catch((error) => console.error(error))
+        .finally(() => console.log("I hope these examples help you!"));
+      /* expected output:
+          error - Learning new JavaScript features is hard
+          I hope these examples help you!
+      */
+    }`;
   }
 }
