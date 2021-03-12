206518de145856cab4fdf154e01bb2eaa7d53bd4
Alba Rivas <arivas@salesforce.com>
2021-03-12 10:57:55 +0100

    Improve for await examples

--- force-app/main/default/lwc/asyncIteration/asyncIteration.js
+++ force-app/main/default/lwc/asyncIteration/asyncIteration.js
@@ -2,49 +2,105 @@ import { LightningElement } from "lwc";
 
 export default class AsyncIteration extends LightningElement {
   async asyncIteration() {
+      async function* fetchLwcRecipesCommits(repo) {
+        let url = "https://api.github.com/repos/lwc-recipes/commits";
+      
+        while (url) {
+          // GitHub API responds with a JSON of 30 commits
+          // providing a link to the next page in the Link header 
+          const response = await fetch(url);
+      
+          const body = await response.json();
+      
+          // Extract URL for the next page
+          let nextPage = response.headers.get('Link').match(/<(.*?)>; rel="next"/);
+          url = (nextPage !== undefined) ? nextPage[1] : undefined;
 
-    function sleep(ms) {
-      return new Promise(resolve => setTimeout(resolve, ms));
-    }
+          for(let commit of body) { //Yield commits one by one, until the page ends
+            yield commit;
+          }
+        }
+      }
 
-    const iterables = [
-      sleep(1000).then(() => { console.log("Promise 1 resolved"); return "Astro";}),
-      sleep(3000).then(() => { console.log("Promise 2 resolved"); return "Codey";}),
-      sleep(2000).then(() => { console.log("Promise 3 resolved"); return "Appy";})
-    ];
+      for await (const commit of fetchLwcRecipesCommits()) {
+        console.log(commit.author.login);
+      }
 
-    const mascots = [];
-    const startTimestamp = Date.now();
-    for await (const obj of iterables) {
-      console.log(`${obj} arrived in ${Date.now() - startTimestamp} ms!`);
-      mascots.push(obj);
-    }
+    /*
+      function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
 
-    console.log(`These mascots went to the party:${mascots.join(',')}`);
+      const iterables = [
+        sleep(1000).then(() => { console.log("Promise 1 resolved"); return "Astro";}),
+        sleep(3000).then(() => { console.log("Promise 2 resolved"); return "Codey";}),
+        sleep(2000).then(() => { console.log("Promise 3 resolved"); return "Appy";})
+      ];
 
-    /* expected ouput
-        Promise 1 resolved
-        Astro arrived in 1000 ms!
-        Promise 3 resolved
-        Promise 2 resolved
-        Codey arrived in 3000 ms!
-        Appy arrived in 3000 ms!
-        These mascots went to the party:Astro,Codey,Appy
+      const mascots = [];
+      const startTimestamp = Date.now();
+      for await (const obj of iterables) {
+        console.log(`${obj} arrived in ${Date.now() - startTimestamp} ms!`);
+        mascots.push(obj);
+      }
+
+      console.log(`These mascots went to the party:${mascots.join(',')}`);
+
+      // expected ouput
+      //  Promise 1 resolved
+      //  Astro arrived in 1000 ms!
+      //  Promise 3 resolved
+      //  Promise 2 resolved
+      //  Codey arrived in 3000 ms!
+      //  Appy arrived in 3000 ms!
+      //  These mascots went to the party:Astro,Codey,Appy
     */
 
     /*
-    const otherIterables = [
-      sleep(1000).then(() => { console.log("Other promise 1 resolved"); return "Cloudy";}),
-      sleep(3000).then(() => { console.log("Other promise 2 resolved"); return "Einstein";}),
-      sleep(2000).then(() => { console.log("Other promise 3 resolved"); return "Blaze";})
-      ]
+      function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
 
-    const startTimestamp2 = Date.now();
-    Promise.all(otherIterables).then(results => {console.log(`${results} arrived in ${Date.now() - startTimestamp2} ms!`)});
+      const otherIterables = [
+        sleep(1000).then(() => { console.log("Other promise 1 resolved"); return "Cloudy";}),
+        sleep(3000).then(() => { console.log("Other promise 2 resolved"); return "Einstein";}),
+        sleep(2000).then(() => { console.log("Other promise 3 resolved"); return "Blaze";})
+        ]
+
+      const startTimestamp2 = Date.now();
+      const mascots2 = [];
+      for await (const obj of await Promise.all(otherIterables)) {
+        console.log(`${obj} arrived in ${Date.now() - startTimestamp2} ms!`);
+        mascots2.push(obj);
+      }
+
+      console.log(`These mascots went to the party:${mascots2.join(',')}`);
     */
   }
 
   get code() {
-    return this.asyncIteration.toString();
+    return `async function* fetchLwcRecipesCommits(repo) {
+      let url = "https://api.github.com/repos/lwc-recipes/commits";
+    
+      while (url) {
+        // GitHub API responds with a JSON of 30 commits
+        // providing a link to the next page in the Link header 
+        const response = await fetch(url);
+    
+        const body = await response.json();
+    
+        // Extract URL for the next page
+        let nextPage = response.headers.get('Link').match(/<(.*?)>; rel="next"/);
+        url = (nextPage !== undefined) ? nextPage[1] : undefined;
+
+        for(let commit of body) { //Yield commits one by one, until the page ends
+          yield commit;
+        }
+      }
+    }
+
+    for await (const commit of fetchLwcRecipesCommits()) {
+      console.log(commit.author.login);
+    }`
   }
 }
